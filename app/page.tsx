ae87e961e95dc38c13df4fecb4869b794bbfb372
DiabGarren <garrendiab@gmail.com>
2026-04-10 10:47:19 +0200

    Removed function import that doesnt exist

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { calcMolarMass, identifyElements } from "@/functions/functions";
+import { calcMolarMass } from "@/functions/functions";
 import { JSX, useState } from "react";
 
 export default function Home() {
